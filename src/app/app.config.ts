9af62d73945793a5f1e04314d2d3cd0cef0941f4
Dakota Pease <peased22@gmail.com>
2023-07-17 11:02:24 -0400

    adds environment files + empty firebase config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
 
 import { routes } from './app.routes';
 import 'zone.js';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(AngularFireModule.initializeApp(environment.firebase)),
+  ],
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+import { IEnvironment } from './IEnvironment';
+
+export const environment: IEnvironment = {
+  firebase: {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+  },
+};
