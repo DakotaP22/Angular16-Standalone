cead5ae57c75dc8ddfaaa9b4200f3f2101a97d09
Dakota Pease <peased22@gmail.com>
2023-08-01 19:52:10 -0400

    Revert 'ignore future environment files'

---
+++ src/environments/IEnvironment.ts
@@ -0,0 +1,12 @@
+export type IFirebaseConfig = {
+  apiKey: string;
+  authDomain: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+};
+
+export interface IEnvironment {
+  firebase: IFirebaseConfig;
+}
